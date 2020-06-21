f7aed14db8a08337b0ee2d902b82f13efaf6f15d
VIMATION <vimal.kum2002@gmail.com>
2020-06-21 14:03:04 +0530

    Data_Vizualization_Assignment

--- Assignment/untitled3.py
+++ Assignment/untitled3.py
@@ -1,9 +1,4 @@
-import numpy as np
-#s=np.array([1,2,3,4])
-#print(s)
-#
-#arr=np.array([[1,1,1],[2,2,2],[3,3,3]])
-#print(arr)
-#print(arr.ndim)
-arr=np.zeros(10,dtype=int)
-print(arr)
+a,b=c=2+2,"tut"
+print(a)
+print(b)
+print(c)
