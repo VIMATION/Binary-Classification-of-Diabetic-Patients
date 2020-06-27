0fecd3fe34fae19bd093247dbdd1ee00cc74cacf
VIMATION <vimal.kum2002@gmail.com>
2020-06-27 20:46:57 +0530

    Data

--- Assignment/untitled3.py
+++ Assignment/untitled3.py
@@ -1,4 +1,10 @@
-a,b=c=2+2,"tut"
-print(a)
-print(b)
-print(c)
+import pandas as pd
+import seaborn as sns
+import numpy as np
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+x=np.linspace(0,10,1000)
+plt.plot(x,np.sin(x),color='black')
+plt.xlabel('x',size=30,color='green')
+plt.ylabel('y',size=30,color='green')
+plt.title('Sine-wave',size=30)
